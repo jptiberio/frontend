63b26d4c12f08dbde90134b45442a2293a2d8436
João Pedro <jptiberio@icloud.com>
2020-01-23 09:50:34 -0300

    Adiciona evento para o clique em entrar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from './services/api';
 import './App.css';
+
 import logo from './assets/logo.png';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+
+    console.log(_id);
+  }
+
+
   return (
     <div className="container">
       <img src={logo} alt="ET" width="200"/>
       <div className="content">
-        <p>Ofereça <strong>naves</strong> para sua tropa e encontre <strong>alies</strong> para em sua jornada</p>
-
-        <form>
+        {/* <p>Ofereça <strong>naves</strong> para sua tropa e encontre <strong>aliens</strong> em sua jornada</p> */}
+        <p>Test first app React react </p>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             type="type" 
             name="Enviar" 
             id="email" 
-            placeholder="Partiu dominar o espaço"
+            placeholder="Dominate world"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
           <button className="btn">Entrar</button>
         </form>
